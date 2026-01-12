0e13b3ae9e2ab62811feebc354b7c75cbc2108e3
Ankit Pathak <ap813ankitpathak@gmail.com>
2026-01-12 10:39:25 +0530

    Add files via upload

---
+++ chapter1modules_comments_pip/ch1_2module_pip_comment.py
@@ -0,0 +1,17 @@
+import pyjokes 
+joke=pyjokes.get_joke()
+print(joke)
+#EXAMPLE 
+#pip install pyjokes in terminal to install
+
+#single line comment 
+# pip install flask in terminal to install
+
+""" 
+types of comment:
+
+ single line 
+ 
+ multi line 
+
+""" 
---
+++ chapter1modules_comments_pip/chapter1practiceset/ch1_q2externalmodule.py
@@ -0,0 +1,5 @@
+# install and external module and use it 
+import pyttsx3
+pyttsx3.speak("good morning coders  ")
+
+#pip install pyttsx3
